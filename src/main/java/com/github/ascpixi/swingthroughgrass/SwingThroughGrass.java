45eda41928b188b02b874deb6dab6d1e243d7ccb
ascpixel <44982772+ascpixi@users.noreply.github.com>
2022-02-18 16:54:54 +0100

    Add bStats Metrics support
    https://bstats.org/

--- src/main/java/com/github/ascpixi/swingthroughgrass/SwingThroughGrass.java
+++ src/main/java/com/github/ascpixi/swingthroughgrass/SwingThroughGrass.java
@@ -1,14 +1,27 @@
 package com.github.ascpixi.swingthroughgrass;
 
 import com.github.ascpixi.swingthroughgrass.listener.GrassSwingListener;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * The main class of the SwingThroughGrass plugin.
  */
 public final class SwingThroughGrass extends JavaPlugin {
+    private static Metrics metrics;
+
+    /**
+     * Gets the Metrics instance for the plugin.
+     * @return The metrics instance, or null if the plugin hasn't been initialized yet
+     */
+    public static Metrics getMetrics(){
+        return metrics;
+    }
+
     @Override
     public void onEnable() {
+        metrics = new Metrics(this, 14356);
+
         getServer()
             .getPluginManager()
             .registerEvents(new GrassSwingListener(), this);
